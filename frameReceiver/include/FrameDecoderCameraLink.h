0499c9f9ddd9537135531fa56bb2ec94048eb1ac
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2021-08-11 14:10:22 +0100

    Add mutex to empty buffer queue handling in FrameDecoderCameraLink

--- frameReceiver/include/FrameDecoderCameraLink.h
+++ frameReceiver/include/FrameDecoderCameraLink.h
@@ -1,6 +1,9 @@
 /*
  * FrameDecoderCameraLink.h
  *
+ * Abstract base class for frameReceiver decoder implementations for CameraLink-based
+ * systems.
+ *
  *  Created on: July 27, 2021
  *      Author: Tim Nicholls, STFC Detector Sytems Software Group
  */
@@ -17,6 +20,7 @@
 
 #include <boost/shared_ptr.hpp>
 #include <boost/function.hpp>
+#include <boost/thread/mutex.hpp>
 
 #include <log4cxx/logger.h>
 using namespace log4cxx;
@@ -27,6 +31,7 @@ using namespace log4cxx::helpers;
 
 namespace FrameReceiver
 {
+
 class FrameDecoderCameraLink : public FrameDecoder
 {
 public:
@@ -36,7 +41,34 @@ public:
   {
   };
 
+  virtual void start_camera_service(std::string& notify_endpoint) = 0;
+  virtual void stop_camera_service(void) = 0;
   virtual ~FrameDecoderCameraLink() = 0;
+
+  void push_empty_buffer(int buffer_id)
+  {
+    boost::lock_guard<boost::mutex> lock(mutex_);
+    FrameDecoder::push_empty_buffer(buffer_id);
+  }
+
+  bool get_empty_buffer(int& buffer_id, void*& buffer_addr)
+  {
+    boost::lock_guard<boost::mutex> lock(mutex_);
+
+    bool buffer_avail = !empty_buffer_queue_.empty();
+
+    if (buffer_avail)
+    {
+      buffer_id = empty_buffer_queue_.front();
+      empty_buffer_queue_.pop();
+      buffer_addr = buffer_manager_->get_buffer_address(buffer_id);
+    }
+
+    return buffer_avail;
+  }
+
+private:
+  boost::mutex mutex_;
 
 };
 
