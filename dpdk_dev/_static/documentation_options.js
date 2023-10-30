add0cea7fed3379190d88a48075631d44f3b2124
timcnicholls <timcnicholls@users.noreply.github.com>
2023-10-30 13:54:44 +0000

    deploy: 8928ab4a18810674fda98a006df0fac7c856f671

--- dpdk_dev/_static/documentation_options.js
+++ dpdk_dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.geb48a4f',
+    VERSION: '0+untagged.1.g8928ab4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -8,7 +8,7 @@ var DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: true,
+    NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
